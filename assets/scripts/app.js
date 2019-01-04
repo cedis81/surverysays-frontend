67aafc9cfc42a0a87f8c70235248b6f7e920bbd7
cedis81 <ccchom11@gmail.com>
2019-01-04 14:40:36 -0500

    Updated app.js file to require auth and survey
    * Require auth events in app.js to create user authentication event
      handlers
    * Require survey events in app.js to create survey CRUD event handlers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,10 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/events')
+const surveyEvents = require('./survey/events')
+
 $(() => {
-  // your JS code goes here
+  authEvents.addAuthHandlers()
+  surveyEvents.addSurveyHandlers()
 })
