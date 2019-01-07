646d888758cbeea5744c7be81bcc802314535714
tommyle90 <tommy.le1004@gmail.com>
2019-01-07 16:16:45 -0500

    Create Answer completed

--- assets/scripts/answer/ui.js
+++ assets/scripts/answer/ui.js
@@ -1,13 +1,14 @@
-const store = require('../store.js')
-const showAllSurveys = require('../templates/allsurvey.handlebars')
-const showMySurveys = require('../templates/mysurvey.handlebars')
+// const store = require('../store.js')
+// const showAllSurveys = require('../templates/allsurvey.handlebars')
+// const showMySurveys = require('../templates/mysurvey.handlebars')
 
-const getAnswerSuccess = (data) => {
-  console.log(data.answers)
-  const showAnswer = showAnswer({ answers: data.answers })
-  $('.content').html(showAnswer)
+const createAnswerSuccess = (data) => {
+  $('#message').html('Question has been answered')
+  $('#message').addClass('success-message')
+  $('#message').removeClass('error-message')
+  $('.survey-answer-create').trigger('reset')
 }
 
 module.exports = {
-  getAnswerSuccess
+  createAnswerSuccess
 }
